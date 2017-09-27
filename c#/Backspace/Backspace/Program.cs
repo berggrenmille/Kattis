247282a5d514d2c5affd67ca9c1357fd34af096c
iceklue <iceklue@gmail.com>
2017-09-27 14:44:13 +0200

    wow

---
+++ c#/Backspace/Backspace/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] inputArr = input.ToCharArray();
+            while(inputArr.Contains('<'))
+            {
+                for (int i = 0; i < inputArr.Length; i++)
+                {
+                    if (inputArr[i] == '<')
+                    {
+                        inputArr[i] = ' ';
+                        inputArr[i - 1] = ' ';
+                        inputArr = (new string(inputArr)).Trim(' ').ToCharArray();
+                        Console.WriteLine(new string(inputArr));
+                        break;
+                    }
+                    
+                }
+            }
+            Console.WriteLine(new string(inputArr));
+            Console.Read();
+        }
+    }
+}
---
+++ c#/PrintedStatues/PrintedStatues/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintedStatues
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int numOfPrinters = 1;
+            int n = Int32.Parse(Console.ReadLine());
+            int countDays = 0;
+
+            if (n == 1)
+                countDays = 1;
+            else if (n % 2 == 0)
+            {
+                numOfPrinters += (n / 2)-1;
+                countDays += (n / 2)-1;
+                if(n % numOfPrinters == 0)
+                    countDays += n / numOfPrinters;
+                else
+                {
+                    countDays += (n / numOfPrinters) + 1;
+                }
+                
+            }
+            else
+            {
+                numOfPrinters += (n / 2);
+                countDays += (n / 2);
+                if (n % numOfPrinters == 0)
+                    countDays += n / numOfPrinters;
+                else
+                {
+                    countDays += (n / numOfPrinters) + 1;
+                }
+            }
+
+            
+
+            Console.WriteLine(countDays);
+            Console.Read();
+
+        }
+    }
+}
